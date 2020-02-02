e1a34485c645e91a102b2c1d43f241f9d007cdea
jswope2112 <jswope2112@gmail.com>
2020-02-02 14:59:10 -0600

    add lane arrivals by normal distribution

--- side.py
+++ side.py
@@ -1,15 +1,18 @@
+import numpy as np
+
 avg_travel_delay = [10, 10, 10]
 
 #Represents one side of an intersection
 class side():
 
-    def __init__(self, sched, name, lanes, arrival_distribution):
+    def __init__(self, sched, name, lanes, arrival_distribution, counter):
         self.sched = sched
         self.name = name
         #self.intersection = intersection # Points to the intersection this side is a part of (CURRENTLY NO LONGER NEEDED)
         self.lanes = lanes
         self.arrival_distribution = arrival_distribution
         self.destinations = [None, None, None]  # Points to the destination Side after going left, straight, or right, respectively
+        self.counter = counter
         
     def depart(self, time, lanes):
     
@@ -41,6 +44,35 @@ class side():
             lane.queue += cars_incoming // len(self.lanes)
         
         print("{} cars arrived at {}. Queue is now {}".format(cars_incoming, self.name, self.queue_to_string()))
+
+    def nave_luckie_dist(self):
+        if self.counter == 0:
+            return int(np.random.normal(451, 25))
+        if self.counter == 1:
+            return int(np.random.normal(299, 25))
+        if self.counter == 2:
+            return int(np.random.normal(953, 50))
+        if self.counter == 3:
+            return int(np.random.normal(800, 50))
+
+
+    def nave_techwood_dist(self):
+        if self.counter == 0:
+            return int(np.random.normal(317, 25))
+        if self.counter == 1:
+            return int(np.random.normal(953, 25))
+        if self.counter == 2:
+            return int(np.random.normal(108, 50))
+        if self.counter == 3:
+            return int(np.random.normal(800, 50))
+
+    def offramp_dist(self):
+        if self.counter == 0:
+            return int(np.random.normal(344, 25))
+        if self.counter == 1:
+            return int(np.random.normal(800, 50))
+        if self.counter == 2:
+            return int(np.random.normal(953, 50))
         
     def set_dests(self, destinations):
         self.destinations = destinations

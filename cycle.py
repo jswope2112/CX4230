85d5ac4b604e585726c26654e5450aeba70abdb9
TeddyShang <tshang3@gatech.edu>
2020-02-02 11:14:37 -0500

    Deleting extra files

--- cycle.py
+++
@@ -1,7 +0,0 @@
-# Represents a single phase of traffic on a given Side of an Intersection
-class Phase:
-
-    def __init__(self, sides, lanes, length):
-        self.sides = side
-        self.lanes = lanes
-        self.length = length
